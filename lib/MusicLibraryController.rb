d0dc2d121c3aa3977b9fb31ca0fec91f78c75224
Marcel Rico <personal.mr4@gmail.com>
2020-01-23 18:37:41 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -60,12 +60,7 @@ class MusicLibraryController
   
   def list_songs_by_artist
     puts "Please enter the name of an artist:"
-    artist_name = gets.chomp
-    artist_obj = Artist.find_by_name(artist_name)
-    if artist_obj != nil
-      songs = artist_obj.songs.sort{|a,b| a.name <=> b.name}
-      songs.each_with_index{|song,idx| puts "#{idx+1}. #{song.name} - #{song.genre.name}"}
-    end
+
   end
   
   def list_songs_by_genre
