5afc91bbd4423c9b984d9a5a1aa5114431fd460b
Marcel Rico <personal.mr4@gmail.com>
2020-01-23 18:02:01 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -70,7 +70,7 @@ class MusicLibraryController
     puts "Which song number would you like to play?"
     user_input = gets.chomp
     if user_input != nil
-      selected_song = Song.all.map{|song| song.name}.sort[3]
+      selected_song = Song.all.sort[3]
       
       binding.pry
     end
