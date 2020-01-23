4aeac2e85231a952b92d6192a162b6f43e4101b9
Marcel Rico <personal.mr4@gmail.com>
2020-01-23 18:12:01 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -70,8 +70,9 @@ class MusicLibraryController
     puts "Which song number would you like to play?"
     total_song_count = Song.all.count
     user_input = gets.chomp
-    selected_song = Song.all.sort{|a,b| a.name <=> b.name}[user_input.to_i - 1]
-    if selected_song != nil
+    
+    if selected_song != nil && user_input < 
+      selected_song = Song.all.sort{|a,b| a.name <=> b.name}[user_input.to_i - 1]
       puts "Playing #{selected_song.name} by #{selected_song.artist.name}"
     end
     
